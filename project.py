80dc4dcae4afe9814c723bb97860a809566aa669
SamadEdwards <124610777+SamadEdwards@users.noreply.github.com>
2023-02-16 14:16:14 +0000

    add

---
+++ project.py
@@ -0,0 +1,22 @@
+import math 
+def greet():
+    print("look closly")
+print("Chose whatevr number you want between 1 and 10") 
+a = int(input())
+print(" now multipy the number you chose by 2")
+greet()
+new_number = a * 2
+print(" then multiply your new number by 5")
+greet()
+new_number_times_5 = new_number * 5
+print(" then divide that number by you orginal number")
+greet()
+super_new_number = new_number_times_5 / a
+print("Now just subtract that number form 7 and")
+greet()
+result = super_new_number - 7
+round(result)
+print("is your number")
+print(result)
+print( "your mind has been read")
+
